ea8038fedc4592525cf3ea1410870f278dfb112d
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-02-10 17:23:58 +0900

    Add files via upload

--- Pong.py
+++ Pong.py
@@ -1,4 +1,4 @@
-import pygame, sys, random,math
+import pygame, sys, random, math, time
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption("Pong Game")
@@ -19,6 +19,7 @@ class Bat:
         self.x = x
         self.y = 260
         self.side = side
+        self.lastbop = 0
 
     def move(self):
         if pressed_keys[self.ctrls[0]] and self.y > 0:
@@ -27,19 +28,26 @@ class Bat:
             self.y += 10
 
     def draw(self):
-        pygame.draw.line(screen,(255,255,255),(self.x,self.y),(self.x,self.y+80),6)
+        offset = -self.side*(time.time() < self.lastbop+0.05) * 30
+        pygame.draw.line(screen,(255,255,255),(self.x + offset,self.y),(self.x + offset,self.y+80),6)
+
+    def bop(self):
+        if time.time() > self.lastbop + 0.3:
+            self.lastbop = time.time()
 
 class Ball:
     def __init__(self):
-        d = (math.pi/3)*random.random()+(math.pi/3)+math.pi*random.choice(lor)
+        self.d = (math.pi/3)*random.random()+(math.pi/3)+math.pi*random.choice(lor)
+        self.speed = 12
         self.fast = random.randint(0,5)
         self.randball = random.randint(0,10)
         
         if self.fast == 1 and self.randball != 1:
-            self.dx = math.sin(d)*24
+            self.speed = 24
         else:
-            self.dx = math.sin(d)*12
-        self.dy = math.cos(d)*12
+            self.speed = 12
+        self.dx = math.sin(self.d)*self.speed
+        self.dy = math.cos(self.d)*self.speed
         self.x = 475
         self.y = 275
 
@@ -48,19 +56,41 @@ class Ball:
         self.y += self.dy
 
     def bounce(self):
-        if self.y <= 0 or self.y >= 550:
-            if self.randball == 1:
+        if (self.y <= 0 and self.dy < 0) or (self.y >= 550 and self.dy > 0):
+            '''if self.randball == 1:
                 d = (math.pi/3)*random.random()+(math.pi/3)+math.pi*random.choice(lor)
                 self.dy = math.cos(d)*12
-            else:
-                self.dy *= -1
+            else:'''
+            self.d = math.atan2(self.dx,self.dy)
+            self.dy *= -1
 
         for bat in bats:
             if pygame.Rect(bat.x,bat.y,6,80).colliderect(self.x,self.y,50,50) and abs(self.dx)/self.dx == bat.side:
-                if self.randball == 1:
+                '''if self.randball == 1:
                     d = (math.pi/3)*random.random()+(math.pi/3)+math.pi*random.choice(lor)
                     self.dx = math.sin(d)*12
-                self.dx *= -1
+                else:'''
+                self.d += random.random()*math.pi/4 - math.pi/8
+                if (0 < self.d < math.pi/6) or (math.pi*5/6 < self.d < math.pi):
+                    self.d = ((math.pi / 3) * random.random() + (math.pi/3))
+                elif (math.pi < self.d < math.pi*7/6) or (math.pi*11/6 < self.d < math.pi*2):
+                    self.d = ((math.pi/3) * random.random()+(math.pi/3))+math.pi
+                    
+                self.d *= -1
+                self.d %= math.pi * 2
+
+                if time.time() < bat.lastbop + 0.05:
+                    if self.speed < 20:
+                        self.speed *= 1.5
+                        print("1")
+                elif self.fast == 1:
+                    self.speed *= 1.3
+                else:
+                    if self.speed < 20:
+                        self.speed *= 1.1
+                
+                self.dx = math.sin(self.d)*self.speed
+                self.dy = math.cos(self.d)*self.speed
 
     def draw(self):
         screen.blit(ball_image,(self.x, self.y))
@@ -74,6 +104,11 @@ while 1:
         if event.type == QUIT:
             sys.exit()
             pygame.quit()
+        if event.type == KEYDOWN:
+            if event.key == K_q:
+                bats[0].bop()
+            if event.key == K_RSHIFT:
+                bats[1].bop()
     pressed_keys = pygame.key.get_pressed()
 
     screen.fill((0,0,0))
