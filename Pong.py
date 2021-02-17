0e5e2b6fb6eec710bc399a865562a28cd3c387e0
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-02-17 17:21:36 +0900

    Add files via upload

--- Pong.py
+++ Pong.py
@@ -11,12 +11,18 @@ ball_image = pygame.image.load("ball.png").convert_alpha()
 rscore = 0
 lscore = 0
 lor = [0, 1]
+mtimer = 0
 
 font = pygame.font.Font(None, 40)
+
 font2 = pygame.font.SysFont("corbel", 70)
 font3 = pygame.font.Font(None, 60)
 font4 = pygame.font.Font(None, 30)
 
+sfont1 = pygame.font.Font(None, 500)
+sfont2 = pygame.font.Font(None, 300)
+
+tfont = pygame.font.Font(None, 50)
 
 class Bat:
     def __init__(self, ctrls, x, side):
@@ -39,7 +45,6 @@ class Bat:
     def bop(self):
         if time.time() > self.lastbop + 0.3:
             self.lastbop = time.time()
-
 
 class Ball:
     def __init__(self):
@@ -66,13 +71,13 @@ class Ball:
         if (self.y <= 0 and self.dy < 0) or (self.y >= 550 and self.dy > 0):
             if self.randball == 1:
                 if self.y <= 0 and self.dy < 0:
-                    self.d = math.pi / 4 * random.random() + math.pi / 4 + (math.pi + math.pi / 4) * random.choice(lor)
+                    self.d = ((2 * math.pi) / 3) * random.random() - ((5 * math.pi) / 12)
 
                     self.dx = math.sin(self.d) * self.speed
                     self.dy = math.cos(self.d) * self.speed
 
                 elif self.y >= 550 and self.dy > 0:
-                    self.d = math.pi / 4 * random.random() + math.pi / 2 + math.pi / 2 * random.choice(lor)
+                    self.d = ((2 * math.pi) / 3) * random.random() + ((7 * math.pi) / 12)
 
                     self.dx = math.sin(self.d) * self.speed
                     self.dy = math.cos(self.d) * self.speed
@@ -85,16 +90,17 @@ class Ball:
             if pygame.Rect(bat.x, bat.y, 6, 80).colliderect(int(self.x), int(self.y), 50, 50) and abs(
                     self.dx) / self.dx == bat.side:
                 if self.randball == 1:
-                    self.d += random.random() * math.pi / 4 - math.pi / 8
+                    if self.x < 20:
+                        self.d = ((2 * math.pi) / 3) * random.random() + (math.pi / 12)
+                        
+                    elif self.x > 940:
+                        self.d = ((2 * math.pi) / 3) * random.random() + (math.pi / 12) + math.pi
 
-                    if (0 < self.d < math.pi / 6) or (math.pi * 5 / 6 < self.d < math.pi):
-                        self.d = ((math.pi / 3) * random.random() + (math.pi / 3))
-                    elif (math.pi < self.d < math.pi * 7 / 6) or (math.pi * 11 / 6 < self.d < math.pi * 2):
-                        self.d = ((math.pi / 3) * random.random() + (math.pi / 3)) + math.pi
-                    self.d %= math.pi * 2
-
-                self.d *= -1
+                else:
+                    self.d *= -1
                 self.dx *= -1
+                    
+                self.d %= math.pi * 2
 
                 if time.time() < bat.lastbop + 0.05:
                     if self.speed < 20:
@@ -113,9 +119,37 @@ class Ball:
     def draw(self):
         screen.blit(ball_image, (int(self.x), int(self.y)))
 
+def start():
+    screen.fill((0,0,0))
+    stxt = sfont1.render("3", True, (255, 255, 255))
+    screen.blit(stxt, (400,150))
+    pygame.display.update()
+    time.sleep(1)
+    
+    screen.fill((0,0,0))
+    stxt = sfont1.render("2", True, (255,255,255))
+    screen.blit(stxt, (400,150))
+    pygame.display.update()
+    time.sleep(1)
+    
+    screen.fill((0,0,0))
+    stxt = sfont1.render("1", True, (255,255,255))
+    screen.blit(stxt, (400,150))
+    pygame.display.update()
+    time.sleep(1)
+
+    screen.fill((0,0,0))
+    stxt = sfont2.render("START", True, (255,255,255))
+    screen.blit(stxt, (180, 180))
+    pygame.display.update()
+    time.sleep(0.5)
 
 ball = Ball()
 bats = [Bat([K_w, K_s], 10, -1), Bat([K_UP, K_DOWN], 984, 1)]
+
+start()
+
+mtimer = time.time()
 
 while 1:
     clock.tick(30)
@@ -123,11 +157,13 @@ while 1:
         if event.type == QUIT:
             sys.exit()
             pygame.quit()
+            
         if event.type == KEYDOWN:
             if event.key == K_q:
                 bats[0].bop()
             if event.key == K_RSHIFT:
                 bats[1].bop()
+                
     pressed_keys = pygame.key.get_pressed()
 
     screen.fill((0, 0, 0))
@@ -157,6 +193,10 @@ while 1:
     txt = font.render(str(rscore), True, (255, 255, 255))
     screen.blit(txt, (980 - txt.get_width(), 20))
 
+    mcount = round(60-(time.time()-mtimer))
+    timetxt = font.render(str(mcount), True, (255,0,0))
+    screen.blit(timetxt, (screen.get_width() * 24 / 50, 25))
+
     if rscore > 9 or lscore > 9:
         screen.fill((0, 0, 0))
 
@@ -171,7 +211,7 @@ while 1:
         screen.blit(txt, (int(screen.get_width() * 3 / 4 - txt.get_width() / 2), int(screen.get_height() / 2)))
 
         txt = font4.render("Press Space to restart", True, (255, 255, 255))
-        screen.blit(txt, (int(screen.get_width() * 5 / 9), int(screen.get_height() - 50)))
+        screen.blit(txt, (int(screen.get_width() * 2 / 5), int(screen.get_height() - 50)))
 
         while 1:
             for event in pygame.event.get():
@@ -184,6 +224,7 @@ while 1:
                 bats[0].y = 200
                 bats[1].y = 200
                 ball = Ball()
+                start()
                 break
             pygame.display.update()
 
